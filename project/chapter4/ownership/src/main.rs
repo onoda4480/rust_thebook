008f9f41e261e082e575d801bdb4f70b74084956
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-07 21:10:21 +0900

    クローンなのでそれぞれ動くが、メモリコストは高め

--- project/chapter4/ownership/src/main.rs
+++ project/chapter4/ownership/src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1;
+let s1 = String::from("hello");
+let s2 = s1.clone();
 
-    println!("{}, world!", s1);
+println!("s1 = {}, s2 = {}", s1, s2);
 }
