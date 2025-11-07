2d6b620213af216d8034a919b04ccdb09a1b582e
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-07 22:37:57 +0900

    ざつスライス

--- project/chapter4/ownership/src/main.rs
+++ project/chapter4/ownership/src/main.rs
@@ -1,9 +1,19 @@
-fn main() {
-    let reference_to_nothing = dangle();
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
 
-fn dangle() -> String {
-    let s = String::from("hello");
+fn main() {
+    let s = String::from("hello world");
+    let word_end = first_word(&s);
 
-    s
+    println!("最初の単語の終わり: {}", word_end);  // 5
+    println!("最初の単語: {}", &s[0..word_end]);  // hello
 }
