0a03979bbca63adde3b9e901edf180cc3569f8be
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-07 22:44:56 +0900

    これはエラー

--- project/chapter4/ownership/src/main.rs
+++ project/chapter4/ownership/src/main.rs
@@ -1,10 +1,21 @@
-#![allow(unused)]
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
 fn main() {
-let s = String::from("hello");
+    let mut s = String::from("hello world");
 
-let len = s.len();
+    let word = first_word(&s);
 
-let slice = &s[0..len];
-let slice = &s[..];
-print!("the slice is: {}", slice);
+    s.clear(); // error! （エラー！）
+
+    println!("the first word is: {}", word);
 }
