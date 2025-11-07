1ee3da9a16da478ae4bb793007b2652632aa9805
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-07 22:22:01 +0900

    ダンダリンぐポイント 何もないものを参照することになるのであかん

--- project/chapter4/ownership/src/main.rs
+++ project/chapter4/ownership/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let mut s = String::from("hello");
+    let reference_to_nothing = dangle();
+}
 
-    let r1 = &s; // 問題なし
-    let r2 = &s; // 問題なし
-    let r3 = &mut s; // 大問題！
+fn dangle() -> &String {
+    let s = String::from("hello");
 
-    println!("{}, {}, {}", r1, r2, r3);  // 全部使う
+    &s
 }
