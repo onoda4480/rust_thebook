bfb3625d243dbeffafea2e6a7ed7fee7aee4892d
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-07 22:43:00 +0900

    これだと最初から最後まで取得

--- project/chapter4/ownership/src/main.rs
+++ project/chapter4/ownership/src/main.rs
@@ -1,11 +1,10 @@
-
 #![allow(unused)]
 fn main() {
 let s = String::from("hello");
 
 let len = s.len();
 
-let slice = &s[3..len];
-let slice = &s[3..];
-println!("{}", slice);
+let slice = &s[0..len];
+let slice = &s[..];
+print!("the slice is: {}", slice);
 }
