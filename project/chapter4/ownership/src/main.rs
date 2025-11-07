6a2ec6dd2da29075961469a87f109576f2fdae39
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-07 22:42:03 +0900

    これも同じ

--- project/chapter4/ownership/src/main.rs
+++ project/chapter4/ownership/src/main.rs
@@ -1,8 +1,11 @@
+
 #![allow(unused)]
 fn main() {
 let s = String::from("hello");
 
-let slice = &s[0..2];
-let slice = &s[..2];
+let len = s.len();
+
+let slice = &s[3..len];
+let slice = &s[3..];
 println!("{}", slice);
 }
