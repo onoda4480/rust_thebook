a50b034be94b4f037d3bbd35d6cdedbe489555e6
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-07 22:40:59 +0900

    文字型スライス

--- project/chapter4/ownership/src/main.rs
+++ project/chapter4/ownership/src/main.rs
@@ -1,19 +1,7 @@
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-
-    s.len()
-}
-
 fn main() {
     let s = String::from("hello world");
-    let word_end = first_word(&s);
 
-    println!("最初の単語の終わり: {}", word_end);  // 5
-    println!("最初の単語: {}", &s[0..word_end]);  // hello
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("{} {}", hello, world);
 }
