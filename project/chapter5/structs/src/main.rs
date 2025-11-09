7e85234ffe6745723927efd21a822502f2c4f078
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-09 18:43:45 +0900

    Eメールとユーザ名を取り、Userインスタンスを返すbuild_user関数

--- project/chapter5/structs/src/main.rs
+++ project/chapter5/structs/src/main.rs
@@ -7,13 +7,12 @@ struct User {
     active: bool,
 }
 
-let mut user1 = User {
-    email: String::from("someone@example.com"),
-    username: String::from("someusername123"),
-    active: true,
-    sign_in_count: 1,
-};
-
-user1.email = String::from("anotheremail@example.com");
-println!("User email: {}", user1.email);
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
 }
