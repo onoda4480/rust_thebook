64fa88cfbb8dbe1dd6ce65b87dc0350870f1a94c
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-09 18:47:02 +0900

    これが面倒だからフィールド初期化省略記法を使っている

--- project/chapter5/structs/src/main.rs
+++ project/chapter5/structs/src/main.rs
@@ -9,10 +9,21 @@ struct User {
 
 fn build_user(email: String, username: String) -> User {
     User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+// ↑でいちいち引の変数名とフィールド名を呼ぶのは面倒だから
+// ↓でフィールド初期化省略記法を使っている
+fn build_user_syokika(email: String, username: String) -> User {
+    User {
         email,
         username,
         active: true,
         sign_in_count: 1,
     }
 }
+
 }
