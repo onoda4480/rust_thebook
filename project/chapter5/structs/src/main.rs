3a9ce85bf63e734ff623b11a454ae252d3b66781
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-09 18:38:09 +0900

    可変なuserインスタンスのemailフィールドの値を変更する

--- project/chapter5/structs/src/main.rs
+++ project/chapter5/structs/src/main.rs
@@ -7,10 +7,12 @@ struct User {
     active: bool,
 }
 
-let user1 = User {
+let mut user1 = User {
     email: String::from("someone@example.com"),
     username: String::from("someusername123"),
     active: true,
     sign_in_count: 1,
 };
+
+user1.email = String::from("anotheremail@example.com");
 }
