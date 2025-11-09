7d410cf42dcc8c151938dd8dd94322e3d77a885b
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-09 18:35:31 +0900

    User構造体のインスタンスを生成する

--- project/chapter5/structs/src/main.rs
+++ project/chapter5/structs/src/main.rs
@@ -6,4 +6,11 @@ struct User {
     sign_in_count: u64,
     active: bool,
 }
+
+let user1 = User {
+    email: String::from("someone@example.com"),
+    username: String::from("someusername123"),
+    active: true,
+    sign_in_count: 1,
+};
 }
