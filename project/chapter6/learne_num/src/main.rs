b922fd3a71c43a9a91b2ebe8b1c92bfc30d5c76b
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 21:16:44 +0900

    if letで書いた場合

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -15,9 +15,9 @@ enum Coin {
 //let coin = Coin::Penny;
 let coin = Coin::Quarter(UsState::Alaska);
 let mut count = 0;
-match coin {
-    // {:?}州のクォーターコイン
-    Coin::Quarter(state) => println!("State quarter from {:?}!", state),
-    _ => count += 1,
+if let Coin::Quarter(state) = coin {
+    println!("State quarter from {:?}!", state);
+} else {
+    count += 1;
 }
 }
