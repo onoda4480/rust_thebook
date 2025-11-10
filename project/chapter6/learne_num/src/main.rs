e9f20ed3c71ac080dbe3e6f011d705254bd8a77e
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 20:39:21 +0900

    コメントを追加

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -9,6 +9,7 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u32 {
     match coin {
+        //メソッドがCoin::Pennyとともに呼び出されるたびに「Lucky penny!」と表示しつつ、 ブロックの最後の値、1を返す
         Coin::Penny => {
             println!("Lucky penny!");
             1
