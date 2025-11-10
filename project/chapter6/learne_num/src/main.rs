586e5c1805084ce638f84862c9417885e43d1fb0
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 21:02:04 +0900

    matchでの_

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -1,14 +1,12 @@
 #![allow(unused)]
 fn main() {
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        Some(i) => Some(i + 1),
-    }
+let some_u8_value = 0u8;
+match some_u8_value {
+    1 => println!("one"),
+    3 => println!("three"),
+    5 => println!("five"),
+    7 => println!("seven"),
+    _ => (),
 }
-//上記は`None`がカバーされていないためエラーを引き起こす。
-//マッチは包括的なので、すべての可能性をカバーする必要がある。
-let five = Some(5);
-let six = plus_one(five);
-let none = plus_one(None);
-println!("{:?}, {:?}, {:?}", six, none, five);
+//_というパターンは、どんな値にもマッチするが、その値を変数に束縛しないことを意味する。
 }
