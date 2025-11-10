bdcaf9c98e5e5adad50bf5454878af6fdff8c312
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 21:07:58 +0900

    if letを使用して同じように書く

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -2,8 +2,7 @@
 fn main() {
 //let some_u8_value = Some(0u8);
 let some_u8_value = Some(3u8);
-match some_u8_value {
-    Some(3) => println!("three"),
-    _ => (),
+if let Some(3) = some_u8_value {
+    println!("three");
 }
 }
