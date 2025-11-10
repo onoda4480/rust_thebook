23e9961cb330563c6f007366d352968c741426a1
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 21:13:21 +0900

    if letではmatchであった強制された包括性チェックは働かない

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -5,4 +5,7 @@ let some_u8_value = Some(3u8);
 if let Some(3) = some_u8_value {
     println!("three");
 }
+//if letではmatchであった強制された包括性チェックは働かない
+//matchかif letかの選択は、 特定の場面でどんなことをしたいかと簡潔性を得ることが
+// 包括性チェックを失うのに適切な代償となるかによる。
 }
