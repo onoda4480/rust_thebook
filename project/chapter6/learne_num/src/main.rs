41dd2295d95084052ef637e89bdf0aacbec44f1e
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 21:00:31 +0900

    マッチは包括的

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -2,11 +2,11 @@
 fn main() {
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
-        None => None,
         Some(i) => Some(i + 1),
     }
 }
-
+//上記は`None`がカバーされていないためエラーを引き起こす。
+//マッチは包括的なので、すべての可能性をカバーする必要がある。
 let five = Some(5);
 let six = plus_one(five);
 let none = plus_one(None);
