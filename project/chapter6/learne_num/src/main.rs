9d153e29176a51bf6143d6ab99a7ddd39b87fd18
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 19:44:29 +0900

    これok

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -4,6 +4,9 @@ enum IpAddrKind {
     V4,
     V6,
 }
-let four = IpAddrKind::V4;
-let six = IpAddrKind::V6;
+
+fn route(ip_type: IpAddrKind) { }
+
+route(IpAddrKind::V4);
+route(IpAddrKind::V6);
 }
