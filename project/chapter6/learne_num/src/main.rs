2016addf4b8e9830f3dc5a826c8a780d077d8b6b
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 20:06:51 +0900

    structで書いた場合を追加

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -7,3 +7,14 @@ enum Message {
     ChangeColor(i32, i32, i32),
 }
 }
+//以下のように定義することもできるがあまりにもめんどくさいのでenumを使うほうが良い
+// #![allow(unused)]
+// fn main() {
+// struct QuitMessage; // ユニット構造体
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String); // タプル構造体
+// struct ChangeColorMessage(i32, i32, i32); // タプル構造体
+// }
