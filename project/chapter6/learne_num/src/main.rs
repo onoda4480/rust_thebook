dca6728f0bac8d592db6c8067ce2ad169044cb97
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 20:25:12 +0900

    noneは明示的にOption<好きな型>で定義しないといけない

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -1,7 +1,11 @@
 #![allow(unused)]
 fn main() {
-enum Option<T> {
-    Some(T),
-    None,
-}
+let some_number = Some(5);
+let some_string = Some("a string");
+
+let absent_number: Option<i32> = None;
+//prelude(初期化処理)にも含まれているので、明示的にuseしなくても使える
+//Noneは明示的にOption<好きな型>(例:Option<i32>)で定義しないといけない
+// Stringなのかi32なのかどんなNoneかがわからないから
+// Some(5)やSome("a string")は中身があるので型推論でわかる
 }
