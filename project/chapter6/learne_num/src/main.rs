e67625c3501943818a818c3049c4706cfb82ef92
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 20:08:30 +0900

    構造体(strct)と同じようにimplでメソッドを定義できる

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -6,6 +6,16 @@ enum Message {
     Write(String),
     ChangeColor(i32, i32, i32),
 }
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+        // メソッド本体はここに定義される
+    }
+}
+
+let m = Message::Write(String::from("hello"));
+m.call();
 }
 //以下のように定義することもできるがあまりにもめんどくさいのでenumを使うほうが良い
 // #![allow(unused)]
