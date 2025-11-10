ffd54e69dfb60d52b5aaa26fbf690e233715bb2e
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 20:38:49 +0900

    波括弧で囲うこともできます。

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -1,15 +1,18 @@
 #![allow(unused)]
 fn main() {
 enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
+   Penny,
+   Nickel,
+   Dime,
+   Quarter,
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
