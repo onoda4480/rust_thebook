37ea2fe8a274a4b10d60c2fc2a076d9bf45b6a98
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 20:04:54 +0900

    列挙子各々が異なる型と量の値を格納する

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -1,15 +1,9 @@
 #![allow(unused)]
 fn main() {
-struct Ipv4Addr {
-    // 省略
-}
-
-struct Ipv6Addr {
-    // 省略
-}
-
-enum IpAddr {
-    V4(Ipv4Addr),
-    V6(Ipv6Addr),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 }
