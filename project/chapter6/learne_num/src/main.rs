8f8019e77426c8b96811a4f27dfe169ea5d1e87c
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 20:14:51 +0900

    Option<T>で定義

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -1,30 +1,7 @@
 #![allow(unused)]
 fn main() {
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+enum Option<T> {
+    Some(T),
+    None,
 }
-
-impl Message {
-    fn call(&self) {
-        // method body would be defined here
-        // メソッド本体はここに定義される
-    }
 }
-
-let m = Message::Write(String::from("hello"));
-m.call();
-}
-//以下のように定義することもできるがあまりにもめんどくさいのでenumを使うほうが良い
-// #![allow(unused)]
-// fn main() {
-// struct QuitMessage; // ユニット構造体
-// struct MoveMessage {
-//     x: i32,
-//     y: i32,
-// }
-// struct WriteMessage(String); // タプル構造体
-// struct ChangeColorMessage(i32, i32, i32); // タプル構造体
-// }
