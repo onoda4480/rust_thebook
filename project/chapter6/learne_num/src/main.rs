4a67dbf0f3ba1efaf934768a24b19ff3f271ea69
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 20:27:14 +0900

    xとyで型が違うからだめ

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -1,11 +1,8 @@
 #![allow(unused)]
 fn main() {
-let some_number = Some(5);
-let some_string = Some("a string");
+let x: i8 = 5;
+let y: Option<i8> = Some(5);
 
-let absent_number: Option<i32> = None;
-//prelude(初期化処理)にも含まれているので、明示的にuseしなくても使える
-//Noneは明示的にOption<好きな型>(例:Option<i32>)で定義しないといけない
-// Stringなのかi32なのかどんなNoneかがわからないから
-// Some(5)やSome("a string")は中身があるので型推論でわかる
+let sum = x + y;
+//i8とOption<i8>が異なる型なので、 足し合わせる方法がコンパイラにはわからないからエラー！！
 }
