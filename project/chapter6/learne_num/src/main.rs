aa6ef8d0add7f76421bf26daa5f55722ef2047cc
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-10 19:58:13 +0900

    v4とv6の型が異なってもおk

--- project/chapter6/learne_num/src/main.rs
+++ project/chapter6/learne_num/src/main.rs
@@ -1,30 +1,11 @@
 #![allow(unused)]
 fn main() {
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
-let home = IpAddr::V4(String::from("127.0.0.1"));
+let home = IpAddr::V4(127, 0, 0, 1);
 
 let loopback = IpAddr::V6(String::from("::1"));
 }
-//以下のように構造体を作らなくていい
-// enum IpAddrKind {
-//     V4,
-//     V6,
-// }
-// struct IpAddr {
-//   kind: IpAddrKind,
-//    address: String,
-// }
-
-// let home = IpAddr {
-//     kind: IpAddrKind::V4,
-//     address: String::from("127.0.0.1"),
-// };
-
-// let loopback = IpAddr {
-//     kind: IpAddrKind::V6,
-//     address: String::from("::1"),
-// };
