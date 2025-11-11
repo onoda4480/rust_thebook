ead4744cd305274e13a1a6ca4dc60ccd516c51c8
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-11 21:32:16 +0900

    ちゃんと使おうとするとこうなる

--- project/chapter7/restaurant/src/main.rs
+++ project/chapter7/restaurant/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
+
+use restaurant::eat_at_restaurant;
 fn main() {
-use std::collections::*;
+    eat_at_restaurant();
 //パスにおいて定義されているすべての公開要素をスコープに持ち込みたいときは、
 // glob演算子 * をそのパスの後ろに続けて書く
 }
