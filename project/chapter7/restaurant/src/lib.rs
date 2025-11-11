99bbb7e60f0e82de96c30bb25513a92a5d73be64
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-11 20:33:39 +0900

    toastは公開されていて、seasonal_fruitは非公開

--- project/chapter7/restaurant/src/lib.rs
+++ project/chapter7/restaurant/src/lib.rs
@@ -1,10 +1,34 @@
-fn serve_order() {}
-
+#![allow(unused)]
+fn main() {
 mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order();
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    fn cook_order() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    // 夏 (summer) にライ麦 (Rye) パン付き朝食を注文
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    // やっぱり別のパンにする
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // 下の行のコメントを外すとコンパイルできない。食事についてくる
+    // 季節のフルーツを知ることも修正することも許されていないので
+    // meal.seasonal_fruit = String::from("blueberries");
+}
 }
