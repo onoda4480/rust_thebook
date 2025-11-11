fedebfc42e175ef14cada75d7fbef550a71c6967
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-11 20:28:07 +0900

    相対パスをsuperで始める

--- project/chapter7/restaurant/src/lib.rs
+++ project/chapter7/restaurant/src/lib.rs
@@ -1,15 +1,10 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
     }
-}
 
-pub fn eat_at_restaurant() {
-    // Absolute path
-    // 絶対パス
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    // 相対パス
-    front_of_house::hosting::add_to_waitlist();
+    fn cook_order() {}
 }
