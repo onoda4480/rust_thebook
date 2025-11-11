430573c6b8cf6353d392054f609ac00fb607895a
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-11 21:00:01 +0900

    エイリアスをつけたら両方フルパスでもおけまる

--- project/chapter7/restaurant/src/lib.rs
+++ project/chapter7/restaurant/src/lib.rs
@@ -1,17 +1,15 @@
 #![allow(unused)]
 fn main() {
-use std::fmt;
-use std::io;
+use std::fmt::Result;
+use std::io::Result as IoResult;
 
-fn function1() -> fmt::Result {
+fn function1() -> Result {
     // --snip--
-    // （略）
     Ok(())
 }
 
-fn function2() -> io::Result<()> {
+fn function2() -> IoResult<()> {
     // --snip--
-    // （略）
     Ok(())
 }
 }
