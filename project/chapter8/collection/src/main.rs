1d55c717910b37e58cc8fb42acfa64ad439172b4
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 08:15:31 +0900

    to_stringメソッドを使用して文字列リテラルからStringを生成する

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,4 +1,10 @@
 #![allow(unused)]
 fn main() {
-let mut s = String::new();
+let data = "initial contents";
+
+let s = data.to_string();
+
+// the method also works on a literal directly:
+let s = "initial contents".to_string();
+println!("{}", s);
 }
