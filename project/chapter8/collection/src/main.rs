fe46321d0091d094fb1ef0ce43ca19ee8056166f
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 08:25:59 +0900

    pushで追加

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,8 +1,7 @@
 #![allow(unused)]
 fn main() {
-let mut s1 = String::from("foo");
-let s2 = "bar";
-s1.push_str(s2);
-println!("s2 is {}", s2);
-//push_strは参照を取るので、s2の所有権は奪わない‼️
+let mut s = String::from("lo");
+s.push('l');
+println!("{}", s);
+//pushは1文字を引数としてとりStringに追加
 }
