d957923d273a1fac1dcbe98daba1115803d1a60b
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 08:02:21 +0900

    enumを使用

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
-        println!("{}", i);
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
-    //*(参照外し)に関しては、
-    // 第15章の「参照外し演算子」を参照してください。
+    // ベクタの各要素が異なる型を持つことができるようにするために、
+    // enumを使用
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
