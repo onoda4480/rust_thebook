74cfaa43f677d9f04067ac8d6a44c2006fcafe02
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 08:13:21 +0900

    新規文字列を作成

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,14 +1,4 @@
+#![allow(unused)]
 fn main() {
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
-    // ベクタの各要素が異なる型を持つことができるようにするために、
-    // enumを使用
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
+let mut s = String::new();
 }
