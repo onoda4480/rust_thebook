bd928d77b8ab3d8577bf9f9131c4f6c55adadd0e
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 08:16:44 +0900

    これも行なっていることは一緒

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,10 +1,4 @@
 #![allow(unused)]
 fn main() {
-let data = "initial contents";
-
-let s = data.to_string();
-
-// the method also works on a literal directly:
-let s = "initial contents".to_string();
-println!("{}", s);
+let s = String::from("initial contents");
 }
