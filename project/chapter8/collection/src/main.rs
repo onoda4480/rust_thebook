25023be8c5dc7a44e2ce8e772340105c56ee9bc1
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 07:53:57 +0900

    forループで要素を走査し、ベクタの各要素を表示する

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
 
-    let first = &v[0];
-
-    v.push(6);
-    // エラー: 可変参照と不変参照の同時使用
-    //解放されたメモリにアクセスしようとしているため、
-    // コンパイルエラーになる。
-    println!("The first element is: {}", first);
 }
