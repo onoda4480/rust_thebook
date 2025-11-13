804f92507c35d9a280396d58b19d943fd4fd9e4a
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 08:39:48 +0900

    Rustの文字列は、添え字アクセスをサポートしていない

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,14 +1,7 @@
 #![allow(unused)]
 fn main() {
-let s1 = String::from("tic");
-let s2 = String::from("tac");
-let s3 = String::from("toe");
-
-let s = format!("{}-{}-{}", s1, s2, s3);
-
-println!("{}", s);
-//format!は所有権を取らないので、s1, s2, s3は有効
-println!("{}", s1);
-println!("{}", s2);
-println!("{}", s3);
+let s1 = String::from("hello");
+let h = s1[0];
+println!("{}", h);
+//Rustの文字列は、添え字アクセスをサポートしていない！
 }
