dd051cf03a0d6ea2c9f4caa93aaac4c8ebe52228
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 08:24:14 +0900

    push_strは所有権を奪わない

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 fn main() {
-let mut s = String::from("foo");
-s.push_str("bar");
-println!("{}", s);
+let mut s1 = String::from("foo");
+let s2 = "bar";
+s1.push_str(s2);
+println!("s2 is {}", s2);
+//push_strは参照を取るので、s2の所有権は奪わない‼️
 }
