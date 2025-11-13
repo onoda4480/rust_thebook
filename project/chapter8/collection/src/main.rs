a906aff8030a8149ffe5dd3d7436af199d4261d3
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 08:37:01 +0900

    format!は所有権を取らないので、s1, s2, s3は有効

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -7,4 +7,8 @@ let s3 = String::from("toe");
 let s = format!("{}-{}-{}", s1, s2, s3);
 
 println!("{}", s);
+//format!は所有権を取らないので、s1, s2, s3は有効
+println!("{}", s1);
+println!("{}", s2);
+println!("{}", s3);
 }
