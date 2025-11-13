c1284395e5dca55841c19ed7b543dc1474ffe209
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 07:45:44 +0900

    ベクタへのアクセス方法

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    {
-        let v = vec![1, 2, 3, 4];
-        println!("ベクタの最初の要素は: {}", v[0]);
-        // vで作業をする
-    } // <- vはここでスコープを抜け、解放される
-    //println!("ベクタはスコープを抜けたので解放されました:{}", v[0]);
-     // エラー: vはもはや有効ではない
+    let v = vec![1, 2, 3, 4, 5];
+
+    //&と[]を使ってベクタの要素にアクセスする方法
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+    
+    //getメソッドを使ってベクタの要素にアクセスする方法
+    match v.get(2) {
+        //                      "3つ目の要素は{}です"
+        Some(third) => println!("The third element is {}", third),
+        //               "3つ目の要素はありません。"
+        None => println!("There is no third element."),
+    }
 }
