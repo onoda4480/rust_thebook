f9b90c8816aedc5687f80cda5b53c8cedc762483
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 07:56:22 +0900

    loopで50たす

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let v = vec![100, 32, 57];
-    for i in &v {
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
         println!("{}", i);
     }
-
+    //*(参照外し)に関しては、
+    // 第15章の「参照外し演算子」を参照してください。
 }
