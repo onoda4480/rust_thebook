75f45bda3efdf770b7039c991935f0bdc64a8f98
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 09:22:03 +0900

    getメソッドでmapから値を取得

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,20 +1,13 @@
 #![allow(unused)]
 fn main() {
 use std::collections::HashMap;
-let field_name = String::from("Favorite color");
-let field_value = String::from("Blue");
 
-let mut map = HashMap::new();
-map.insert(field_name, field_value);
-// field_name and field_value are invalid at this point, try using them and
-// see what compiler error you get!
-// field_nameとfield_valueはこの時点で無効になる。試しに使ってみて
-// どんなコンパイルエラーが出るか確認してみて！
-//以下はアクセス方法が違うのでエラー
-println!("{}", map["Favorite color"]);
-println!("{}", map["Blue"]);
+let mut scores = HashMap::new();
 
-//これはmapに所有権が移動しているのでエラー
-println!("{}", field_name);
-println!("{}", field_value);
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Yellow"), 50);
+
+let team_name = String::from("Blue");
+let score = scores.get(&team_name);
+println!("{:?}", score);
 }
