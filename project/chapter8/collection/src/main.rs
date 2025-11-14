09e2755c912be6b03fa4ddfb54f4f6dbebbfa05b
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 09:08:27 +0900

    collectメソッドによるHashMapの作成

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -1,9 +1,13 @@
 #![allow(unused)]
 fn main() {
 use std::collections::HashMap;
+//タプルのベクタに対してcollectメソッドを呼び出すことで、
+// ハッシュマップを生成できる:
+let teams  = vec![String::from("Blue"), String::from("Yellow")];
+let initial_scores = vec![10, 50];
 
-let mut scores = HashMap::new();
-
-scores.insert(String::from("Blue"), 10);
-scores.insert(String::from("Yellow"), 50);
+let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+//ベクタのように、ハッシュマップは均質です: 
+// キーは全て同じ型でなければならず、 
+// 値も全て同じ型でなければなりません。
 }
