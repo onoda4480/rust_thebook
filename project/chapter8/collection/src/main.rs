902baa200ab7524eebeb02c83b816f562a08af08
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-14 09:23:21 +0900

    ベクタのようにforloopで走査できる

--- project/chapter8/collection/src/main.rs
+++ project/chapter8/collection/src/main.rs
@@ -7,7 +7,7 @@ let mut scores = HashMap::new();
 scores.insert(String::from("Blue"), 10);
 scores.insert(String::from("Yellow"), 50);
 
-let team_name = String::from("Blue");
-let score = scores.get(&team_name);
-println!("{:?}", score);
+for (key, value) in &scores {
+    println!("{}: {}", key, value);
+}
 }
