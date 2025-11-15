12e627fb90d80a8c3e8b4b1cff31da8353e31fed
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-15 18:33:56 +0900

    match式を使用して返却される可能性のあるResult列挙子を処理する

--- project/chapter9/error_process/src/main.rs
+++ project/chapter9/error_process/src/main.rs
@@ -1,5 +1,13 @@
 use std::fs::File;
 
 fn main() {
-    let f: u32 = File::open("hello.txt");
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            // ファイルを開く際に問題がありました
+            panic!("There was a problem opening the file: {:?}", error)
+        },
+    };
 }
