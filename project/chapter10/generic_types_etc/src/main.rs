7002b1b8d5e16c5d1ccfc76ba8bc3d07a518c1cb
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 21:26:40 +0900

    resultは内側のスコープの終わりまで有効な何かを参照している

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    // 長い文字列は長い
+    let string1 = String::from("long string is long");
+    // （訳注：この言葉自体に深い意味はない。下の"xyz"より長いということだけが重要）
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        // 一番長い文字列は{}
+        println!("The longest string is {}", result);
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
