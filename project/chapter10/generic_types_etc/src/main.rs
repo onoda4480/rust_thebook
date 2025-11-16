54a9505dd162c85b56af301800207816e56de0cf
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 19:02:36 +0900

    ジェネリックな型引数Tに対して特定の具体的な型がある構造体にのみ適用されるimplブロック

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,18 +1,13 @@
+#![allow(unused)]
+fn main() {
 struct Point<T> {
     x: T,
     y: T,
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
 }
-
-fn main() {
-    let p = Point { x: 5, y: 10 };
-
-    println!("p.x = {}", p.x());
-    println!("p.y = {}", p.y);
-    //p.xはメソッドでアクセス、p.yはフィールドで直接アクセスしている
 }
