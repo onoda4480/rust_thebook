a562aa11c8ebd38e9812cabc05e2118499d7ebc8
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 21:01:17 +0900

    トレイト境界によってジェネリックな型に対するメソッド実装を条件分けする

--- project/chapter10/generic_types_etc/src/lib.rs
+++ project/chapter10/generic_types_etc/src/lib.rs
@@ -1,71 +1,22 @@
-pub trait Summary {
-    fn summarize(&self) -> String;
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
 }
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
     }
 }
 
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
     }
 }
-
-fn returns_summarizable() -> impl Summary {
-    Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
-        reply: false,
-        retweet: false,
-    }
-}//具体的な型が何かを言うことなく、
-//returns_summarizable関数は
-//Summaryトレイトを実装している何らかの型を返すのだ、と指定することができる
-
-//以下はエラーになる
-//NewsArticleとTweetは異なる型のため
-//impl Traitは一種類の型を返す場合にのみ使える。
-
-// fn returns_summarizable(switch: bool) -> impl Summary {
-//     if switch {
-//         NewsArticle {
-//             headline: String::from(
-//                 "Penguins win the Stanley Cup Championship!",
-//             ),
-//             location: String::from("Pittsburgh, PA, USA"),
-//             author: String::from("Iceburgh"),
-//             content: String::from(
-//                 "The Pittsburgh Penguins once again are the best \
-//                  hockey team in the NHL.",
-//             ),
-//         }
-//     } else {
-//         Tweet {
-//             username: String::from("horse_ebooks"),
-//             content: String::from(
-//                 "of course, as you probably already know, people",
-//             ),
-//             reply: false,
-//             retweet: false,
-//         }
-//     }
-// }
