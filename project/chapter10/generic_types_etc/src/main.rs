e998223f2430c77edbd9e7309efd5b395c2f916c
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 21:38:53 +0900

    longestでresultのスコープ外になるのでエラー

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     let string1 = String::from("abcd");
-    let string2 = "efghijklmnopqrstuvwxyz";
+    let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
 }
 
-fn longest<'a>(x: &'a str, y: &str) -> &'a str {
-    x
-    //引数xと戻り値に対してライフタイム引数'aを指定しましたが、引数yには指定していません。 
-    //yのライフタイムはxや戻り値のライフタイムとは何の関係もないからです。
+fn longest<'a>(x: &str, y: &str) -> &'a str {
+    // 本当に長い文字列
+    let result = String::from("really long string");
+    result.as_str()
 }
