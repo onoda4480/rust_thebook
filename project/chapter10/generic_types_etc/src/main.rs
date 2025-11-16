b6f0301bd999620ce7f934f2dabfca3a6c9a50af
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 20:11:39 +0900

    summarizeメソッドのデフォルト実装があるSummaryトレイトの定義

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,15 +1,20 @@
-use generic_types_etc::{Summary, Tweet};
+use generic_types_etc::{Summary, NewsArticle, Tweet};
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
+    let article = NewsArticle {
+        // ペンギンチームがスタンレーカップチャンピオンシップを勝ち取る！
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        // アメリカ、ペンシルベニア州、ピッツバーグ
+        location: String::from("Pittsburgh, PA, USA"),
+        // アイスバーグ
+        author: String::from("Iceburgh"),
+        // ピッツバーグ・ペンギンが再度NHL(National Hockey League)で最強のホッケーチームになった
         content: String::from(
-            // もちろん、ご存知かもしれませんがね、みなさん
-            "of course, as you probably already know, people",
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
         ),
-        reply: false,
-        retweet: false,
     };
 
-    println!("1 new tweet: {}", tweet.summarize());
+    println!("New article available! {}", article.summarize());
+
 }
