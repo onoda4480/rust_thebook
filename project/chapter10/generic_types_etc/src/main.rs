c8a17548478e616fec37380a41cc577c2d74953a
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 21:14:11 +0900

    返している参照がxかyのどちらを参照しているかわからんのでエラー

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
-    {
-        let x = 5;            // ----------+-- 'b
-                              //           |
-        let r = &x;           // --+-- 'a  |
-                              //   |       |
-        println!("r: {}", r); //   |       |
-                              // --+       |
-    }                         // ----------+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
