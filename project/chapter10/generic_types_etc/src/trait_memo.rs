783a55f76f985336defa11d78685f64a7cd4cec4
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 20:41:09 +0900

    トレイト境界について

---
+++ project/chapter10/generic_types_etc/src/trait_memo.rs
@@ -0,0 +1,22 @@
+//トレイト境界
+pub fn notify<T: Summary>(item: &T) {
+    // 速報！ {}
+    println!("Breaking news! {}", item.summarize());
+}
+//簡単なケースではimpl Trait構文を使えばいいが、
+//より複雑なケースではトレイト境界を使う方がいい場合がある
+
+//例
+pub fn notify(item1: &impl Summary, item2: &impl Summary) {
+    //
+    println!("Breaking news! {}", item1.summarize());
+    //
+    println!("Breaking news! {}", item2.summarize());
+}
+//上記の引数が冗長なので、トレイト境界を使うと以下のようにできる
+pub fn notify<T: Summary>(item1: &T, item2: &T) {
+    //
+    println!("Breaking news! {}", item1.summarize());
+    //
+    println!("Breaking news! {}", item2.summarize());
+}
