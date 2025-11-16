eec4b76001d8cfdb75f4d97f46f221c3c20dd3a0
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 18:50:07 +0900

    ジェネリック型でそれぞれ違う型として定義できる

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,17 +1,17 @@
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0 };
-    let char_point = Point { x: 'a', y: 'b' };
-    println!("integer Point: ({}, {})", integer.x, integer.y);
-    println!("float Point: ({}, {})", float.x, float.y);
-    println!("char Point: ({}, {})", char_point.x, char_point.y);
-    //両方同じ方でないといけない
-    //以下はエラー
-    //let wont_work = Point { x: 5, y: 4.0 };
-    //intとfloatでそれぞれ型が違うのでエラー
+    // ジェネリック型でそれぞれ違う型として定義できる
+    // Point<T, U>のTとUにそれぞれ異なる型を指定できる
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    let char_and_float = Point { x: 'a', y: 4.0 };
+    println!("both_integer: ({}, {})", both_integer.x, both_integer.y);
+    println!("both_float: ({}, {})", both_float.x, both_float.y);
+    println!("integer_and_float: ({}, {})", integer_and_float.x, integer_and_float.y);
+    println!("char_and_float: ({}, {})", char_and_float.x, char_and_float.y);
 }
