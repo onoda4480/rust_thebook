37d8234eb63c1fff2d26d222a4b57800ea5244d8
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 21:33:39 +0900

    yのライフタイムはxや戻り値のライフタイムとは何の関係もないからok

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,17 +1,13 @@
 fn main() {
-    let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
+    let string1 = String::from("abcd");
+    let string2 = "efghijklmnopqrstuvwxyz";
+
+    let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+fn longest<'a>(x: &'a str, y: &str) -> &'a str {
+    x
+    //引数xと戻り値に対してライフタイム引数'aを指定しましたが、引数yには指定していません。 
+    //yのライフタイムはxや戻り値のライフタイムとは何の関係もないからです。
 }
