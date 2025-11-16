d0731795f7a4f0c37c5d4434427d086d2a20d8db
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 19:35:02 +0900

    ジェネリック型でもコンパイル時のコストは発生しない

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,22 +1,14 @@
-struct Point<T, U> {
-    x: T,
-    y: U,
+enum Option_i32 {
+    Some(i32),
+    None,
 }
 
-impl<T, U> Point<T, U> {
-    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
-    }
+enum Option_f64 {
+    Some(f64),
+    None,
 }
 
 fn main() {
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c'};
-
-    let p3 = p1.mixup(p2);
-
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
 }
