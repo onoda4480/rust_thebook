2a94cb443dd974db86e3930d6b1f40d48e7f7b7b
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 21:15:57 +0900

    ライフタイム注釈記法

---
+++ project/chapter10/generic_types_etc/src/lifetime_memo.rs
@@ -0,0 +1,6 @@
+&i32        // a reference
+            // (ただの)参照
+&'a i32     // a reference with an explicit lifetime
+            // 明示的なライフタイム付きの参照
+&'a mut i32 // a mutable reference with an explicit lifetime
+            // 明示的なライフタイム付きの可変参照
