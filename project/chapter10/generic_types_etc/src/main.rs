5947aa96b4baf42d0305b866133f9faee87bbd3f
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 22:00:09 +0900

    ジェネリックな型引数、トレイト境界、ライフタイム指定の構文のすべてを1つの関数に

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,14 +1,30 @@
-struct ImportantExcerpt<'a> {
-    part: &'a str,
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest_with_an_announcement(
+        string1.as_str(),
+        string2,
+        "Today is someone's birthday!",
+    );
+    println!("The longest string is {}", result);
 }
 
-fn main() {
-    // 僕をイシュマエルとお呼び。何年か前・・・
-    let novel = String::from("Call me Ishmael. Some years ago...");
-    //                                                  "'.'が見つかりませんでした"
-    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-    let i = ImportantExcerpt {
-        part: first_sentence,
-    };
-    println!("Excerpt: {}", i.part);
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    //       "アナウンス！ {}"
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
