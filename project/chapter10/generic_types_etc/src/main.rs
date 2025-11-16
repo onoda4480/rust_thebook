8f54c29a95d277bf42f83ae7c6294e83f98203e4
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-16 21:07:59 +0900

    ダングリング参照のためエラー

--- project/chapter10/generic_types_etc/src/main.rs
+++ project/chapter10/generic_types_etc/src/main.rs
@@ -1,27 +1,12 @@
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
-//        ^^^^^^^^^^^^^^^              ^^
-//        Copy 不要！                  参照を返す
-    let mut largest = &list[0];  // 参照
-    //                ^
-    
-    for item in list {  // item は &T（参照）
-//      ^^^^            &item ではない
-        if item > largest {
-            largest = item;
-        }
-    }
-    
-    largest  // 参照を返す
-}
-
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    {
+        let r;
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+        {
+            let x = 5;
+            r = &x;
+        }
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+        println!("r: {}", r);
+    }
 }
