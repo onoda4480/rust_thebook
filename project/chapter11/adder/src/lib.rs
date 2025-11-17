c0600423d966067386fbe1390adb4baa96296f5c
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-18 08:13:47 +0900

    テストエラー時にコメントを出力させる

--- project/chapter11/adder/src/lib.rs
+++ project/chapter11/adder/src/lib.rs
@@ -10,6 +10,11 @@ mod tests {
     #[test]
     fn greeting_contains_name() {
         let result = greeting("Carol");
-        assert!(result.contains("Carol"));
+        assert!(
+            result.contains("Carol"),
+            //挨拶(greeting)は名前を含んでいません。その値は`{}`でした
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
