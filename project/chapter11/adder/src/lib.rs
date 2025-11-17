4a9321d992ad7a11367717a9c488e3395780eda9
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-18 07:55:15 +0900

    パニックを起こさせたのでテストは失敗する

--- project/chapter11/adder/src/lib.rs
+++ project/chapter11/adder/src/lib.rs
@@ -11,4 +11,10 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+    
+    #[test]
+    fn another() {
+        //このテストを失敗させる
+        panic!("Make this test fail");
+    }
 }
