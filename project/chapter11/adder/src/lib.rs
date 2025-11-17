33ad4aca344b9a7a6d65464ebff41f18808b1d11
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-18 08:45:50 +0900

    #[ignore]でテストを除外

--- project/chapter11/adder/src/lib.rs
+++ project/chapter11/adder/src/lib.rs
@@ -1,27 +1,13 @@
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+#[test]
+fn it_works() {
+    assert_eq!(2 + 2, 4);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn add_two_and_two() {
-        assert_eq!(4, add_two(2));
-    }
-
-    #[test]
-    fn add_three_and_two() {
-        assert_eq!(5, add_two(3));
-    }
-
-    #[test]
-    fn one_hundred() {
-        assert_eq!(102, add_two(100));
-    }
+#[test]
+#[ignore]
+fn expensive_test() {
+    // 実行に1時間かかるコード
+    // code that takes an hour to run
 }
-//cargo test one_hundredやcargo test add_two_and_twoのように
-//特定のテストだけを実行できる
-//cargo test addのようにでテスト関数名の一部を指定して実行することもできる
-//上記の場合はaddで始まるテスト関数がすべて実行される
+
+//cargo test -- --ignoredで無視されるテストのみ実行できる
