d88bf30c230a6cd8dc6e7cd3c13048d214614db4
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-18 08:42:29 +0900

    単独のテストを走らせる

--- project/chapter11/adder/src/lib.rs
+++ project/chapter11/adder/src/lib.rs
@@ -1,7 +1,5 @@
-fn prints_and_returns_10(a: i32) -> i32 {
-    //{}という値を得た
-    println!("I got the value {}", a);
-    10
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[cfg(test)]
@@ -9,19 +7,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn this_test_will_pass() {
-        let value = prints_and_returns_10(4);
-        assert_eq!(10, value);
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
     }
 
     #[test]
-    fn this_test_will_fail() {
-        let value = prints_and_returns_10(8);
-        assert_eq!(5, value);
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
     }
 }
-
-//cargo test -- --nocaptureで実行すると
-//テスト成功時のprintln!の出力が見れる
-//cargo test -- --nocapture --test-threads=1だと並列数を1に制限することで
-//出力が混ざらないようにしている
+//cargo test one_hundredやcargo test add_two_and_twoのように
+//特定のテストだけを実行できる
