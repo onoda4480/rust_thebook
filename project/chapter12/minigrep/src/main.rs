b5b42eb8c94946fe9c82f6e46629b48d3e908c5f
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-19 09:22:04 +0900

    残りのプログラムロジックを含むrun関数を抽出する

--- project/chapter12/minigrep/src/main.rs
+++ project/chapter12/minigrep/src/main.rs
@@ -17,15 +17,15 @@ fn main() {
     // {}というファイルの中
     println!("In file {}", config.filename);
 
-    // ファイルが見つかりませんでした
+    run(config);
+}
+
+fn run (config: Config) {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        // ファイルの読み込み中に問題がありました
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
-    // テキストは\n{}です
     println!("With text:\n{}", contents);
 }
 
