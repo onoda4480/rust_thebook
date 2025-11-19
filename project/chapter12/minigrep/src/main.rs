e195466d07107805ae267ae728a9718aa2cccc29
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-19 09:14:03 +0900

    新しいConfig作成に失敗したら、エラーコードで終了する

--- project/chapter12/minigrep/src/main.rs
+++ project/chapter12/minigrep/src/main.rs
@@ -5,7 +5,11 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     // {}を探しています
     println!("Searching for {}", config.query);
@@ -31,6 +35,7 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Result<Config, &'static str> {
+        //&'はライフタイム注釈
         if args.len() < 3 {
         // 引数の数が足りません
             return Err("not enough arguments");
