02104720b1dcfd7dd5fef207ebebe4c7f48f4a4f
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-20 19:31:52 +0900

    実行に約2秒かかる架空の計算の代役を務める関数

---
+++ project/chapter13/closures/src/main.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+fn main() {
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    // ゆっくり計算します
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+}
