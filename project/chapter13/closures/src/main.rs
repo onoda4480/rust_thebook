0982aa922e5f49ad158398280848853840108441
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-20 19:33:01 +0900

    ユーザ入力や乱数生成をシミュレートするハードコードされた値があるmain関数

--- project/chapter13/closures/src/main.rs
+++ project/chapter13/closures/src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-fn main() {
 use std::thread;
 use std::time::Duration;
 
@@ -9,4 +7,13 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     thread::sleep(Duration::from_secs(2));
     intensity
 }
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
 }
