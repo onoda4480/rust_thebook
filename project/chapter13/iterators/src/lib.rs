b177caab7fa27bdc36946f02ebd6d10d2149d052
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-20 21:17:23 +0900

    nextメソッド実装の機能をテストする

--- project/chapter13/iterators/src/lib.rs
+++ project/chapter13/iterators/src/lib.rs
@@ -20,3 +20,15 @@ impl Iterator for Counter {
         }
     }
 }
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
