3c0c079548300fd54894c199ea36b047428f7457
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-20 20:03:38 +0900

    Cacherのキャッシュ機構

--- project/chapter13/closures/src/main.rs
+++ project/chapter13/closures/src/main.rs
@@ -7,6 +7,28 @@ struct Cacher<T>
     value: Option<u32>,
 }
 
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
     //クロージャでは、fn関数のように引数の型や戻り値の型を注釈する必要はなし
     let expensive_closure = |num: u32| -> u32  {
