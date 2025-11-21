b431931b602b34acc8b2c94424e8e4a37069de58
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-21 20:32:01 +0900

    kindsとutilsモジュールに体系化される要素を含むartライブラリ。 cargo docでドキュメントのページ開く

--- project/chapter14/cargo_and_crates/src/lib.rs
+++ project/chapter14/cargo_and_crates/src/lib.rs
@@ -1,21 +1,35 @@
-//! # My Crate
+//! # Art
 //!
-//! `my_crate` is a collection of utilities to make performing certain
-//! calculations more convenient.
+//! A library for modeling artistic concepts.
+//! #芸術
+//!
+//! 芸術的な概念をモデル化するライブラリ。
 
-//! #自分のクレート
-//!
-//! `my_crate`は、ユーティリティの集まりであり、特定の計算をより便利に行うことができます。
-/// Adds one to the number given.
-/// 与えられた数値に1を足す。
-///
-/// # Examples
-///
-/// ```
-/// let five = 5;
-///
-/// assert_eq!(6, my_crate::add_one(5));
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    /// RYBカラーモデルによる主色
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    /// RYBカラーモデルによる副色
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    ///2つの主色を同じ割合で混合し、副色にする
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+    }
 }
