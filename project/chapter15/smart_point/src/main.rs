f2b9b50be24e70d3579065731b27ff11de8be1d0
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-22 20:02:58 +0900

    MyBox<T>にDerefを実装する

--- project/chapter15/smart_point/src/main.rs
+++ project/chapter15/smart_point/src/main.rs
@@ -5,6 +5,16 @@ impl<T> MyBox<T> {
         MyBox(x)
     }
 }
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
