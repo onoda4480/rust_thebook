856100bddaddb4e362dbc35934f831855da0e2d0
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-22 19:53:25 +0900

    参照外し

--- project/chapter15/smart_point/src/main.rs
+++ project/chapter15/smart_point/src/main.rs
@@ -1,13 +1,9 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
-use List::{Cons, Nil};
-
 fn main() {
-    let list = Cons(1,
-        Box::new(Cons(2,
-            Box::new(Cons(3,
-                Box::new(Nil))))));
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    //assert_eq!(5, y);だとコンパイルエラーになる
+    //比較するには一旦yの参照を外せば、yが指している整数値にアクセスできる。
 }
