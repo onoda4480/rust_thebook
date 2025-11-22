0b11e8bce3beb988f6fdcbb8ae740cf114559829
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-22 22:07:23 +0900

    Cons列挙子が参照しているものを変更できるようにRefCell<T>を抱えているコンスリストの定義

--- project/chapter15/smart_point/src/main.rs
+++ project/chapter15/smart_point/src/main.rs
@@ -1,24 +1,18 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use List::{Cons, Nil};
+
 #[derive(Debug)]
 enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
 }
 
-use List::{Cons, Nil};
-use std::rc::Rc;
-use std::cell::RefCell;
-
-fn main() {
-    let value = Rc::new(RefCell::new(5));
-
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
-
-    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
-    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
-
-    *value.borrow_mut() += 10;
-
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match *self {
+            Cons(_, ref item) => Some(item),
+            Nil => None,
+        }
+    }
 }
