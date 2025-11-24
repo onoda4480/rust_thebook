b4793161fde59971b2168f00e9e5b39998be2469
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-24 19:18:02 +0900

    簡潔性のためにMutex<T>のAPIをシングルスレッドの文脈で探究する

--- project/chapter16/thread/src/main.rs
+++ project/chapter16/thread/src/main.rs
@@ -1,55 +1,12 @@
-use std::thread;
-use std::sync::mpsc;
-use std::time::Duration;
+use std::sync::Mutex;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let m = Mutex::new(5);
 
-    let tx1 = mpsc::Sender::clone(&tx);
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-    let tx2 = mpsc::Sender::clone(&tx);
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hello"),
-            String::from("dear"),
-            String::from("stranger"),
-            String::from("kun"),
-        ];
-
-        for val in vals {
-            tx2.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    thread::spawn(move || {
-        // 君のためにもっとメッセージを(more messages for you)
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
     }
+
+    println!("m = {:?}", m);
 }
