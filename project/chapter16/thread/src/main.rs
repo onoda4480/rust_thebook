94a755eeb0ef9c4cb99db0d1c75f72f482d78d6d
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-24 18:31:00 +0900

    thread::spawnのJoinHandleを保存してスレッドが完了するのを保証する

--- project/chapter16/thread/src/main.rs
+++ project/chapter16/thread/src/main.rs
@@ -2,17 +2,17 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
-            // やあ！立ち上げたスレッドから数字{}だよ！
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
     for i in 1..5 {
-        // メインスレッドから数字{}だよ！
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
