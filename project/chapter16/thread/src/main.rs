a41fc56dee1dddfd652469d6312ea25cb24992c0
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-24 18:42:39 +0900

    moveキーワードを使用してクロージャに使用している値の所有権を強制的に奪わせる

--- project/chapter16/thread/src/main.rs
+++ project/chapter16/thread/src/main.rs
@@ -3,12 +3,9 @@ use std::thread;
 fn main() {
     let v = vec![1, 2, 3];
 
-    let handle = thread::spawn(|| {
+    let handle = thread::spawn(move || {
         println!("Here's a vector: {:?}", v);
     });
-
-    // いや〜！
-    drop(v); // oh no!
 
     handle.join().unwrap();
 }
