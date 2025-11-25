fa26e5fdafff7e2a0ea5416dc91f9dd2cc0ca893
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-25 20:27:43 +0900

    Drawトレイトを実装するあるButton構造体

--- project/chapter16/thread/src/lib.rs
+++ project/chapter16/thread/src/lib.rs
@@ -1,12 +1,16 @@
-pub struct Screen<T: Draw> {
-    pub components: Vec<T>,
+pub trait Draw {
+    fn draw(&self);
 }
 
-impl<T> Screen<T>
-    where T: Draw {
-    pub fn run(&self) {
-        for component in self.components.iter() {
-            component.draw();
-        }
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+        // 実際にボタンを描画するコード
     }
 }
