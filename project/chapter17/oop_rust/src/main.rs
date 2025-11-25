727762dd056e2ce98a1f0ade9577cebe8aa062f7
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-25 20:37:41 +0900

    guiを使用し、SelectBox構造体にDrawトレイトを実装する別のクレート

--- project/chapter17/oop_rust/src/main.rs
+++ project/chapter17/oop_rust/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+extern crate gui;
+use gui::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        //セレクトボックスを実際に描画するコード
+    }
 }
