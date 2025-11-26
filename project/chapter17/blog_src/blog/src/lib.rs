0c41755e816c16909a13cbd6de3ab35f7d4472b5
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-26 09:36:12 +0900

    contentメソッドのあるPostとcontentメソッドのないDraftPost

--- project/chapter17/blog_src/blog/src/lib.rs
+++ project/chapter17/blog_src/blog/src/lib.rs
@@ -1,68 +1,25 @@
 pub struct Post {
-    state: Option<Box<State>>,
+    content: String,
+}
+
+pub struct DraftPost {
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost {
             content: String::new(),
         }
     }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
-    }
-    pub fn content(&self) -> &str {
-        ""
-    }
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
-    }
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
-        }
-    }
-}
-
-trait State {
-    fn request_review(self: Box<Self>) -> Box<State>;
-    fn approve(self: Box<Self>) -> Box<State>;
-}
-
-struct Draft {}
-
-impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<State> {
-        Box::new(PendingReview {})
-    }
-    fn approve(self: Box<Self>) -> Box<State> {
-        self
-    }
-}
-
-struct PendingReview {}
-
-impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<State> {
-        Box::new(Published {})
-    }
-}
-
-struct Published {}
-
-impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<State> {
-        self
     }
 }
