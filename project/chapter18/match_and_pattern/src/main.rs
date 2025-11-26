03862baf02ceb279ff54b2883f6fde365b8cab4b
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 08:36:45 +0900

    letではなく、if letと論駁可能なパターンを含むブロックを使用する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
     let some_option_value: Option<i32> = None;
-    let Some(x) = some_option_value;
-    // コンパイルエラー: `x`はスコープ外です
-    //パターンが論駁可能であることを意味する
-
-    //論駁不可能なもの。例：let x = 5;のx
-    //論駁可能なもの。例：let Some(x) = some_option_value;のx
+    if let Some(x) = some_option_value {
+        println!("{}", x);
+    }
 }
