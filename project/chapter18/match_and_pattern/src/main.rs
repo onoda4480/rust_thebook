3efddf29793ccadecc8927f085976105e5e74c90
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 08:22:04 +0900

    コメント追加

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -4,5 +4,8 @@ let v = vec!['a', 'b', 'c'];
 
 for (index, value) in v.iter().enumerate() {
     println!("{} is at index {}", value, index);
+    //enumerateメソッドを使用してイテレータを改造し、
+    //値とその値のイテレータでの添え字をタプルに配置して生成
+    //enumerateの最初の呼び出しは、タプル(0, 'a')を生成
 }
 }
