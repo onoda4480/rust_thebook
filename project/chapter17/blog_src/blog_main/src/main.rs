8de8ee5b996e33fb04270fc1bb3acc249c5d3d25
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-26 09:40:41 +0900

    ブログ記事ワークフローの新しい実装を使うmainの変更

--- project/chapter17/blog_src/blog_main/src/main.rs
+++ project/chapter17/blog_src/blog_main/src/main.rs
@@ -5,5 +5,10 @@ fn main() {
     let mut post = Post::new();
 
     post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
