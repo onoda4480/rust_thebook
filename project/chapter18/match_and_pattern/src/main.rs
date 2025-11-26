3b5e94c5ca6c08dd265903fcfba36e82ccd3bd1a
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 08:48:46 +0900

    シャドーイングされた変数yを導入するアームのあるmatch式

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,11 +1,17 @@
-#![allow(unused)]
 fn main() {
-let x = 1;
+    let x = Some(5);
+    let y = 10;
 
-match x {
-    1 => println!("one"),       // 1
-    2 => println!("two"),       // 2
-    3 => println!("three"),     // 3
-    _ => println!("anything"),  // なんでも
-}
+    match x {
+        // 50だったよ
+        Some(50) => println!("Got 50"),
+        // マッチしたよ
+        //このyは新しいy変数であり、最初に値10で宣言したyではない
+        Some(y) => println!("Matched, y = {:?}", y),
+        // 既定のケース
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    // 最後にはx = {}, y = {}
+    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
