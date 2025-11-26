2869011d60908c8e17290777098953fecdf507dd
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 08:33:04 +0900

    letで論駁可能なパターンを使用しようとする

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,16 +1,8 @@
-fn foo(x: i32) {
-    //関数シグニチャが引数にパターンを使用している例
-    // コードがここに来る
-    // code goes here
-}
-
-fn print_coordinates(&(x, y): &(i32, i32)) {
-    // 現在の位置: ({}, {})
-    //値&(3, 5)はパターン&(x, y)と合致する
-    println!("Current location: ({}, {})", x, y);
-}
-
 fn main() {
-    let point = (3, 5);
-    print_coordinates(&point);
+    let Some(x) = some_option_value;
+    // コンパイルエラー: `x`はスコープ外です
+    //パターンが論駁可能であることを意味する
+
+    //論駁不可能なもの。例：let x = 5;のx
+    //論駁可能なもの。例：let Some(x) = some_option_value;のx
 }
