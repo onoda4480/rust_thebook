f04624909972ddc2f732189e124589424e82d296
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 08:45:00 +0900

    リテラルにマッチする

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,5 +1,11 @@
+#![allow(unused)]
 fn main() {
-    if let x = 5{
-        println!("x: {}", x);
-    };
+let x = 1;
+
+match x {
+    1 => println!("one"),       // 1
+    2 => println!("two"),       // 2
+    3 => println!("three"),     // 3
+    _ => println!("anything"),  // なんでも
+}
 }
