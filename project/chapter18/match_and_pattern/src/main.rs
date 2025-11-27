5f652d33b2ab943f964612f6c9f9f04e7fda695e
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 09:02:05 +0900

    分配とリテラル値との一致を1つのパターンで行う

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -6,9 +6,12 @@ struct Point {
 fn main() {
     let p = Point { x: 0, y: 7 };
 
-    let Point { x, y } = p;
-    assert_eq!(0, x);
-    assert_eq!(7, y);
-    //let Point { x: a, y: b } = p;
-    //より簡潔に書ける
+    match p {
+        // x軸上の{}
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        // y軸上の{}
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        // どちらの軸上でもない: ({}, {})
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
 }
