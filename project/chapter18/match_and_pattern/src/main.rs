189549a58f6e31f43b76f9182df67e1a448d291c
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 09:29:20 +0900

    アンダースコアを使用すると、値を束縛しない

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,10 +1,8 @@
+#![allow(unused)]
 fn main() {
-// こんにちは！
 let s = Some(String::from("Hello!"));
 
-if let Some(_s) = s {
-    //アンダースコアで始まる未使用の変数は値を束縛し、値の所有権を奪う可能性がある
-    // 文字列が見つかりました
+if let Some(_) = s {
     println!("found a string");
 }
 
