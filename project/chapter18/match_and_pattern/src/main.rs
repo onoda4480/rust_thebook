c56bd2dea5d582292064cc56933cfff58110be6d
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 09:24:25 +0900

    Some内の値を使用する必要がない時にSome列挙子と合致するパターンでアンダースコアを使用する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,8 +1,20 @@
-fn foo(_: i32, y: i32) {
-    // このコードは、y引数を使うだけです: {}
-    println!("This code only uses the y parameter: {}", y);
+#![allow(unused)]
+fn main() {
+let mut setting_value = Some(5);
+let new_setting_value = Some(10);
+
+match (setting_value, new_setting_value) {
+    //中身が両方ともSomeであることを確認したい場合
+    //Some(_)は中身を無視するワイルドカードとして機能します
+    (Some(_), Some(_)) => {
+        // 既存の値の変更を上書きできません
+        println!("Can't overwrite an existing customized value");
+    }
+    _ => {
+        setting_value = new_setting_value;
+    }
 }
 
-fn main() {
-    foo(3, 4);
+// 設定は{:?}です
+println!("setting is {:?}", setting_value);
 }
