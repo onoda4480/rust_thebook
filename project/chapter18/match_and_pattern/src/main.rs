9a727a5d062a993008e0b9012ea94603714903e9
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 09:13:39 +0900

    構造体への参照を構造体のフィールド値に分配する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,36 +1,18 @@
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+#![allow(unused)]
+fn main() {
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-fn main() {
-    let msg = Message::ChangeColor(0, 160, 255);
+let points = vec![
+    Point { x: 0, y: 0 },
+    Point { x: 1, y: 5 },
+    Point { x: 10, y: -3 },
+];
 
-    match msg {
-        Message::Quit => {
-            // Quit列挙子には分配すべきデータがない
-            println!("The Quit variant has no data to destructure.")
-        },
-        Message::Move { x, y } => {
-            println!(
-                // x方向に{}、y方向に{}だけ動く
-                "Move in the x direction {} and in the y direction {}",
-                x,
-                y
-            );
-        }
-        // テキストメッセージ: {}
-        Message::Write(text) => println!("Text message: {}", text),
-        Message::ChangeColor(r, g, b) => {
-            println!(
-                // 色を赤{}, 緑{}, 青{}に変更
-                "Change the color to red {}, green {}, and blue {}",
-                r,
-                g,
-                b
-            )
-        }
-    }
+let sum_of_squares: i32 = points
+    .iter()
+    .map(|&Point { x, y }| x * x + y * y)
+    .sum();
 }
