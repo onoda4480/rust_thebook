be4d87cc099f8eebfebe81ab725f45601aa28c4e
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 09:25:33 +0900

    タプルの複数の部分を無視する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,20 +1,11 @@
 #![allow(unused)]
 fn main() {
-let mut setting_value = Some(5);
-let new_setting_value = Some(10);
+let numbers = (2, 4, 8, 16, 32);
 
-match (setting_value, new_setting_value) {
-    //中身が両方ともSomeであることを確認したい場合
-    //Some(_)は中身を無視するワイルドカードとして機能します
-    (Some(_), Some(_)) => {
-        // 既存の値の変更を上書きできません
-        println!("Can't overwrite an existing customized value");
-    }
-    _ => {
-        setting_value = new_setting_value;
-    }
+match numbers {
+    (first, _, third, _, fifth) => {
+        // 何らかの数値: {}, {}, {}
+        println!("Some numbers: {}, {}, {}", first, third, fifth)
+    },
 }
-
-// 設定は{:?}です
-println!("setting is {:?}", setting_value);
 }
