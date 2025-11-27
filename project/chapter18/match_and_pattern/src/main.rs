9f53df8eafe09e86ddf039152336f4e5450330dc
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 09:19:51 +0900

    構造体とタプルを分配する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -5,15 +5,5 @@ struct Point {
     y: i32,
 }
 
-let points = vec![
-    Point { x: 0, y: 0 },
-    Point { x: 1, y: 5 },
-    Point { x: 10, y: -3 },
-];
-
-let sum_of_squares: i32 = points
-    .iter()
-    .map(|&Point { x, y }| x * x + y * y)
-    .sum();
-println!("Sum of squares: {}", sum_of_squares);
+let ((feet, inches), Point {x, y}) = ((3, 10), Point { x: 3, y: -10 });
 }
