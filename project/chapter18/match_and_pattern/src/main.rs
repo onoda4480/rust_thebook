4dc045082d5e93a4cb82fa4706ea9e222d3c0af1
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-27 09:08:48 +0900

    異なる種類の値を保持するenumの列挙子を分配する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,17 +1,36 @@
-struct Point {
-    x: i32,
-    y: i32,
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
 fn main() {
-    let p = Point { x: 0, y: 7 };
+    let msg = Message::ChangeColor(0, 160, 255);
 
-    match p {
-        // x軸上の{}
-        Point { x, y: 0 } => println!("On the x axis at {}", x),
-        // y軸上の{}
-        Point { x: 0, y } => println!("On the y axis at {}", y),
-        // どちらの軸上でもない: ({}, {})
-        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    match msg {
+        Message::Quit => {
+            // Quit列挙子には分配すべきデータがない
+            println!("The Quit variant has no data to destructure.")
+        },
+        Message::Move { x, y } => {
+            println!(
+                // x方向に{}、y方向に{}だけ動く
+                "Move in the x direction {} and in the y direction {}",
+                x,
+                y
+            );
+        }
+        // テキストメッセージ: {}
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!(
+                // 色を赤{}, 緑{}, 青{}に変更
+                "Change the color to red {}, green {}, and blue {}",
+                r,
+                g,
+                b
+            )
+        }
     }
 }
