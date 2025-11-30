fbae2099aaa1c51bde00625af6cf28233d5858d5
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-30 18:38:11 +0900

    タプルの最初と最後の値にだけ合致し、他の値を無視する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,15 +1,9 @@
-#![allow(unused)]
 fn main() {
-struct Point {
-    x: i32,
-    y: i32,
-    z: i32,
-}
+    let numbers = (2, 4, 8, 16, 32);
 
-let origin = Point { x: 0, y: 0, z: 0 };
-
-match origin {
-    Point { x, .. } => println!("x is {}", x),
-    //match式でx座標のみ処理し、 yとzフィールドの値は無視する
-}
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        },
+    }
 }
