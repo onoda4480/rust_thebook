9922f080625f5a5c845b7ca916b99c4a6ec30744
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-30 19:31:56 +0900

    マッチガードを使用して外側の変数と等しいか確認する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let num = Some(4);
+    let x = Some(5);
+    let y = 10;
 
-    match num {
-        // 5未満です: {}
-        Some(x) if x < 5 => println!("less than five: {}", x),
-        Some(x) => println!("{}", x),
-        None => (),
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {:?}", n),
+        //Some(y)だと外側のyがシャドーイング(覆い隠される)されるため
+        //y==yとなり常に真になってしまう
+        _ => println!("Default case, x = {:?}", x),
     }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
