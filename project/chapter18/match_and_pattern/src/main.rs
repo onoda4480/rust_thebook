b616d6ca522680adabcc656a0442b7e32fe535a7
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-30 19:11:20 +0900

    ref mutを使用して、パターンの一部として値への可変参照を生成する

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-let robot_name = Some(String::from("Bors"));
+    let mut robot_name = Some(String::from("Bors"));
 
-match robot_name {
-    Some(ref name) => println!("Found a name: {}", name),
-    None => (),
-}
+    match robot_name {
+        // 別の名前
+        Some(ref mut name) => *name = String::from("Another name"),
+        None => (),
+    }
 
-println!("robot_name is: {:?}", robot_name);
+    println!("robot_name is: {:?}", robot_name);
 }
