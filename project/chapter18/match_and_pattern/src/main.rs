17772ba49ba2a9ba65467b8c042c14d588839cdc
onoda4480 <mianqiangarutimetto@gmail.com>
2025-11-30 18:57:49 +0900

    matchアームパターンで変数を生成すると、値の所有権が奪われる

--- project/chapter18/match_and_pattern/src/main.rs
+++ project/chapter18/match_and_pattern/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let numbers = (2, 4, 8, 16, 32);
+let robot_name = Some(String::from("Bors"));
 
-    match numbers {
-        (.., second, ..) => {
-            println!("Some numbers: {}", second)
-        },
-    }
+match robot_name {
+    // 名前が見つかりました: {}
+    Some(name) => println!("Found a name: {}", name),
+    None => (),
 }
-//..を使うのは明確しないといけない
-//どの値がマッチしてどの値が無視されるべきかが不明瞭なら
-//コンパイラはエラー
+
+// robot_nameは: {:?}
+println!("robot_name is: {:?}", robot_name);
+}
