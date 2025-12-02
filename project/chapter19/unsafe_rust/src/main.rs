7b1efc1e0ae7f1f9e57f781d671fdafd1a8ce90c
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-03 08:46:36 +0900

    不変で静的な変数を定義し、使用する

--- project/chapter19/unsafe_rust/src/main.rs
+++ project/chapter19/unsafe_rust/src/main.rs
@@ -1,14 +1,7 @@
-unsafe extern "C" {
-    fn abs(input: i32) -> i32;
-    // C標準ライブラリのabs関数を宣言
-    //thebookだとunsafeなしで宣言していたが、
-    //Rust 1.63以降はunsafeが必要らしい
-}
+static HELLO_WORLD: &str = "Hello, world!";
+//const HELLO_WORLD: &str = "Hello, world!"; ←これは定数
 
 fn main() {
-    unsafe {
-        // -3の絶対値は、Cによると{}
-        // unsafeブロック内で呼び出す必要がある
-        println!("Absolute value of -3 according to C: {}", abs(-3));
-    }
+    // 名前は: {}
+    println!("name is: {}", HELLO_WORLD);
 }
