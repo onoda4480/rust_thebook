ca4bf018d0a0f49105c0a4068e7fbf9450c54d3e
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-03 08:11:41 +0900

    任意のメモリアドレスへの生ポインタを生成する

--- project/chapter19/unsafe_rust/src/main.rs
+++ project/chapter19/unsafe_rust/src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused)]
 fn main() {
-    let mut num = 5;
-
-    let r1 = &num as *const i32;
-    let r2 = &mut num as *mut i32;
+    let address = 0x012345usize;
+    let r = address as *const i32;
+    //メモリが確保されていないアドレスを参照しようとしているため、
+    //そのアドレスにデータがある可能性もあるし、ない可能性もあるので、
+    //安全ではない操作として扱われる
 }
