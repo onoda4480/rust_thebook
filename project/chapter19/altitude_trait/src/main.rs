6a580c02f321f390e04721419f7c57fb04bfcef2
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-03 09:39:57 +0900

    Addトレイトを実装してPointインスタンス用に+演算子をオーバーロードする

--- project/chapter19/altitude_trait/src/main.rs
+++ project/chapter19/altitude_trait/src/main.rs
@@ -1,33 +1,26 @@
-pub trait Iterator {
-    type Item;
+use std::ops::Add;
 
-    fn next(&mut self) -> Option<Self::Item>;
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
-//ジェネリクスでトレイトを定義
-pub trait Iterator<T> {
-    fn next(&mut self) -> Option<T>;
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
 }
-//例
-// Counter に対して複数の実装が可能！
-// impl Iterator<u32> for Counter {
-//     fn next(&mut self) -> Option<u32> { /* ... */ }
-// }
 
-// impl Iterator<String> for Counter {
-//     fn next(&mut self) -> Option<String> { /* ... */ }
-// }
-
-// impl Iterator<char> for Counter {
-//     fn next(&mut self) -> Option<char> { /* ... */ }
-// }
-//ジェネリクスだと使う時に混乱する
-//そのため関数型の方が明確
-//// ❌ どの Iterator を使えばいい？
-// counter.next()
-// //      ^^^^ Iterator<u32>? Iterator<String>? Iterator<char>?
-// //           型注釈が必要！
-
-// // 型注釈が必要
-// let x: Option<u32> = counter.next();
-// // または
-// Iterator::<u32>::next(&mut counter);
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+    //+でaddが動いている
+}
