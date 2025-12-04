5c157abaadcca806efcae937a518a6da784d6fea
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-04 09:15:21 +0900

    Vec<String>の周りにWrapperを作成してDisplayを実装する

--- project/chapter19/altitude_trait/src/main.rs
+++ project/chapter19/altitude_trait/src/main.rs
@@ -1,23 +1,15 @@
 use std::fmt;
-struct Point {
-    x: i32,
-    y: i32,
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+        //今回のWrapper構造体はタプルなので添字でアクセスしている
+    }
 }
 
-trait OutlinePrint: fmt::Display {
-    fn outline_print(&self) {
-        let output = self.to_string();
-        let len = output.len();
-        println!("{}", "*".repeat(len + 4));
-        println!("*{}*", " ".repeat(len + 2));
-        println!("* {} *", output);
-        println!("*{}*", " ".repeat(len + 2));
-        println!("{}", "*".repeat(len + 4));
-    }
-}
-//PointにはDisplayがないので実装しないと上記のOutlinePrintトレイトを使えない
-impl fmt::Display for Point {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
+fn main() {
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
