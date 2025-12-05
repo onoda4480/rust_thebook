7df08d424b2dc24d998cbf7e187e463c009f884a
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-05 21:27:34 +0900

    手続き的マクロの使用例

--- project/chapter19/macro_pord/src/lib.rs
+++ project/chapter19/macro_pord/src/lib.rs
@@ -1,12 +1,4 @@
-#[macro_export]
-macro_rules! vec {
-    ( $( $x:expr ),* ) => {
-        {
-            let mut temp_vec = Vec::new();
-            $(
-                temp_vec.push($x);
-            )*
-            temp_vec
-        }
-    };
-}
+use proc_macro;
+
+#[some_attribute]
+pub fn some_name(input: TokenStream) -> TokenStream {}
