55aeccd460022b7dc7b05f4330a199a6d73175d4
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-05 21:36:07 +0900

    パースされたRustコードを用いてHelloMacroトレイトを実装する

--- project/chapter19/macro_pord/hello_macro/src/lib.rs
+++ project/chapter19/macro_pord/hello_macro/src/lib.rs
@@ -1,3 +1,11 @@
-pub trait HelloMacro {
-    fn hello_macro();
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
 }
