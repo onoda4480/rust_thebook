ec4b235202e05c2e2c5d9b381c017f41d28b58ad
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-06 20:01:42 +0900

    ThreadPoolを変更してJobインスタンスを送信するチャンネルの送信側を格納する

--- project/chapter20/hello/src/lib.rs
+++ project/chapter20/hello/src/lib.rs
@@ -1,8 +1,11 @@
+use std::sync::mpsc;
 use std::thread;
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
-
+struct Job;
 impl ThreadPool {
     /// 新しいThreadPoolを生成する。
     ///
@@ -22,13 +25,15 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             workers.push(Worker::new(id));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
     pub fn execute<F>(&self, f: F)
     where
