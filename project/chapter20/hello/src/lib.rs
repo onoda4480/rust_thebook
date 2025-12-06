c90dfeb9295bb686901e17bdcf1ae8bc0c4474d5
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-06 19:58:02 +0900

    ThreadPoolを変更してスレッドを直接保持するのではなく、Workerインスタンスを保持する

--- project/chapter20/hello/src/lib.rs
+++ project/chapter20/hello/src/lib.rs
@@ -21,18 +21,29 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            // スレッドを生成してベクタに格納する
-            // create some threads and store them in the vector
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
