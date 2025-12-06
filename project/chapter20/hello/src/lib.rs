906489b482f8c1853f251591c423bfd89fefe8dc
onoda4480 <mianqiangarutimetto@gmail.com>
2025-12-06 19:43:55 +0900

    ThreadPool::newを実装してsizeが0ならパニックする

--- project/chapter20/hello/src/lib.rs
+++ project/chapter20/hello/src/lib.rs
@@ -1,7 +1,24 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// 新しいThreadPoolを生成する。
+    ///
+    /// sizeがプールのスレッド数です。
+    ///
+    /// # パニック
+    ///
+    /// sizeが0なら、`new`関数はパニックします。
+    ///
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         ThreadPool
     }
     pub fn execute<F>(&self, f: F)
